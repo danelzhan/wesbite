23349bf0207e94d7f3d6ee3149d71421e7a290cc
danelzhan <164102172+danelzhan@users.noreply.github.com>
2024-07-31 23:43:38 +0800

    test git

---
+++ Website/landing.js
@@ -0,0 +1,52 @@
+// landing
+const yesButton = document.getElementById('yes');
+const noButton = document.getElementById('no');
+const textBox = document.getElementById('text');
+const websiteButton = document.getElementById('website');
+
+yesButton.addEventListener("click", function() {
+    landingLogicYes();
+});
+
+noButton.addEventListener("click", function() {
+    landingLogicNo();
+});  
+
+function landingLogicYes () {
+
+    yesButton.style.transform = "scale(1, 1)";
+    noButton.style.transform = "scale(1, 1)";
+    noAmount = 0;
+
+    if (textBox.innerHTML == "hi ivy!") {
+        textBox.innerHTML = "i have a question";
+        yesButton.innerHTML = "ok";
+        noButton.innerHTML = "k...";
+    } else if (textBox.innerHTML == "i have a question") {
+        textBox.innerHTML = "are we dating?";
+        yesButton.innerHTML = "yess :D";
+        noButton.innerHTML = "no i hate u";
+    } else if (textBox.innerHTML == "are we dating?") {
+        textBox.innerHTML = "yayy ok since we're dating and its national girlfriend day i wanna get u a gift";
+        yesButton.innerHTML = "okay!!";
+        noButton.innerHTML = "nah i'm good";
+    } else if (noButton.innerHTML == "nah i'm good") {
+        textBox.innerHTML = "so i made u this rrly cool website go look! (turn up sound and scroll pls)";
+        yesButton.style.display = "none";
+        noButton.style.display = "none";
+        websiteButton.style.display = "flex";
+    }
+}
+
+var noAmount = 0;
+function landingLogicNo () {
+    if (textBox.innerHTML == "hi ivy!" || textBox.innerHTML == "are we dating?" || noButton.innerHTML == "nah i'm good") {
+        noAmount++;
+        yesButton.style.transform = "scale(" + Math.pow(1.5, noAmount) + ", " + Math.pow(1.5, noAmount) + ")";
+        noButton.style.transform = "scale(" + Math.pow(0.75, noAmount) + ", " + Math.pow(0.75, noAmount) + ")";
+    } else if (textBox.innerHTML == "i have a question") {
+        textBox.innerHTML = "are we dating?";
+        yesButton.innerHTML = "yess :D";
+        noButton.innerHTML = "no i hate u";
+    }
+}
---
+++ Website/main.js
@@ -0,0 +1,200 @@
+import './style.css';
+
+import * as THREE from 'three';
+
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// scene setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 500, 100000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+  alpha: true,
+});
+
+renderer.setClearColor( 0x000000, 0 );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(-6000);
+camera.position.setX(-12000);
+camera.position.setY(2000);
+
+// loading assets
+
+const loader = new GLTFLoader();
+
+var record_player = null;
+var japanese_room = null;
+var table = null;
+var cake = null;
+var record = null;
+var candle = null;
+var fire = null;
+
+loader.load(
+  'Record player.glb',
+  function (glb) {
+    console.log(glb);
+    record_player = glb.scene;
+    record_player.position.y = 0;
+    record_player.position.x = -1700;
+    record_player.position.z = 750;
+    record_player.rotation.y = 5.2;
+    scene.add(record_player);
+  }
+);
+
+loader.load(
+  'record.glb',
+  function (glb) {
+    record = glb.scene;
+    record.position.x = -825;
+    record.position.z = 2600;
+    record.position.y = 260;
+    record.scale.multiplyScalar(0.001);
+  }
+);
+
+loader.load(
+  'JapaneseRoom.glb',
+  function (glb) {
+    japanese_room = glb.scene;
+    japanese_room.scale.multiplyScalar(12000);
+    japanese_room.position.x = 0;
+    japanese_room.position.z = 0;
+    japanese_room.position.y = 600;
+    scene.add(japanese_room);
+  }
+);
+
+loader.load(
+  'Table.glb',
+  function (glb) {
+    table = glb.scene;
+    table.scale.multiplyScalar(1200);
+    table.position.x = -3500;
+    table.position.z = -2500;
+    table.position.y = -1650;
+    table.rotation.y = 5.075;
+    scene.add(table);
+  }
+);
+
+loader.load(
+  'Cake.glb',
+  function (glb) {
+    console.log('table');
+    cake = glb.scene;
+    cake.scale.multiplyScalar(1200);
+    cake.position.x = -3500;
+    cake.position.z = -2500;
+    cake.position.y = -700;
+    scene.add(cake);
+  }
+);
+
+loader.load(
+  'Candle.glb',
+  function (glb) {
+    console.log('table');
+    candle = glb.scene;
+    candle.scale.multiplyScalar(1200);
+    candle.position.x = -3500;
+    candle.position.z = -2500;
+    candle.position.y = -350;
+    scene.add(candle);
+  }
+);
+
+loader.load(
+  'Fire.glb',
+  function (glb) {
+    fire = glb.scene;
+    fire.scale.multiplyScalar(80);
+    fire.position.x = -3500;
+    fire.position.z = -2500;
+    fire.position.y = -250;
+    scene.add(fire);
+  }
+);
+
+/*scene.add(new THREE.GridHelper(5000, 100));*/
+ 
+const light = new THREE.HemisphereLight( 0xffeaf6, 0x080808, 2);
+scene.add(light);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.dispose();
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+ // controls.update();
+}
+
+animate();
+
+// scroll animations
+var prevT = 1000;
+var tempT = 0;
+
+var tempX = 0;
+var tempY = 0;
+var tempZ = 0;
+
+function moveCamera() {
+
+  var t = document.body.getBoundingClientRect().top;
+
+  if (camera.position.z <= -4475 || (t > prevT && t > tempT)) {
+    camera.position.x = -12000 + t * -5.1;
+    camera.position.y = 2000 + t * 1.08;
+    camera.position.z = -6000 + t * -1;
+  
+    camera.rotation.y = -1.0857465 + t * -0.0005;
+    camera.rotation.z = -2.8548361 + t * 0.00011;
+
+    tempT = t;
+    tempX = camera.position.x;
+    tempY = camera.position.y;
+    tempZ = camera.position.z;
+  } else if (camera.position.y > -4003 || (t > prevT)) {
+    
+    // camera.position.x = tempX + (t - tempT) * - 5.1;
+    camera.position.y = tempY + (t - tempT) * -0.4;
+    camera.position.z = tempZ + (t - tempT) * -1.5;
+  }
+
+  console.log(camera.position.x);
+  console.log(camera.position.y);
+  console.log("z:" + camera.position.z);
+  prevT = t;
+}
+
+console.log("camera rotation z: " + camera.rotation.z);
+
+document.body.onscroll = moveCamera;
+
+// interactive stuff
+
+//play songs
+const paperRings = document.getElementById('songButton');
+const paperRingsSong = new Audio('paperRings.mp3');
+paperRings.addEventListener("click", function () {
+  paperRingsSong.play();
+  scene.add(record);
+});
+
+//blow candle
+const candleBlow = document.getElementById('blowButton');
+const blowAudio = new Audio('blow.mp3');
+candleBlow.addEventListener("click", function () {
+  blowAudio.play();
+  scene.remove(fire);
+});
+
